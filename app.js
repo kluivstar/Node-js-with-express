15c0c2d3ea53360c7ec88c3779f99166b8143a09
Dev Denzel <100281304+devdenzel@users.noreply.github.com>
2024-08-14 13:57:06 +0100

    handling route parameters

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const app = express()
 let movies = JSON.parse(fs.readFileSync("./data/movies.json"))
 
 app.use(express.json())
+
+
 // GET - api/movies
 app.get('/movies', (req, res)=>{
     res.status(200).json({
@@ -16,10 +18,24 @@ app.get('/movies', (req, res)=>{
         }
     })
 })
+
+app.get('/movies/:id', (req, res)=>{
+    console.log(req.params)
+    res.send('Created something')
+})
 // POST - api/movies
 app.post('/movies', (req, res) => {
-    console.log(req.body)
-    res.send('Created')
+    const newID = movies[movies.length - 1].id + 1
+    const newMovie = Object.assign({id: newID}, req.body)
+    movies.push(newMovie)
+    fs.writeFile('/data/movies.json', JSON.stringify(movies), (err)=>{
+        res.status(201).json({
+            status: "success",
+            data: {
+                movies:newMovie
+            }
+        })
+    })
 })
 // Create Server
 const port = 3000
