d60a6919f588d568c01d8ca017fb7354c1955b61
Dev Denzel <100281304+devdenzel@users.noreply.github.com>
2024-08-16 14:24:31 +0100

    used PATCH to update existing resource

--- app.js
+++ app.js
@@ -23,8 +23,9 @@ app.get('/movies', (req, res)=>{
 
 // GET Request to Retrieve a Single Movie by ID
 app.get('/movies/:id', (req, res) =>{
-    // The id is extracted from the URL (req.params.id) and converted to a number using * 1 // FIND MOVIE BASED ON PRODUCT ID PARAMETER
+    // The id is extracted from the URL (req.params.id) and converted to a number using 
     const id = req.params.id * 1
+    // FIND MOVIE BASED ON PRODUCT ID PARAMETER
     let movie = movies.find(el => el.id === id)
 
     // IF MOVIE IS NOT FOUND
@@ -57,7 +58,23 @@ app.post('/movies', (req, res) => {
         })
     })
 })
+app.patch('/movies/:id', (req, res) => {
+    let id = req.params.id * 1
+    let movieToUpdate  = movies.find(el => el.id === id)
+    let index = movies.indexOf(movieToUpdate)
+    Object.assign(movieToUpdate, req.body)
 
+    movies[index] = movieToUpdate
+    
+    fs.writeFile("./data/movies.json", JSON.stringify(movies), (err)=>{
+        res.status(200).json({
+            status: "success",
+            data: {
+                movie: movieToUpdate
+            }
+        })
+    })
+})
 // Create Server
 const port = 3000
 app.listen(port, ()=>{
