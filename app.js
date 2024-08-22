23f82988fb0a448f269ae803ecc48854b5526444
Dev Denzel <100281304+devdenzel@users.noreply.github.com>
2024-08-22 11:59:12 +0100

    further refactored the api

--- app.js
+++ app.js
@@ -9,9 +9,8 @@ let movies = JSON.parse(fs.readFileSync("./data/movies.json"))
 // Middleware that parse incoming JSON request
 app.use(express.json())
 
-
-// GET - api/movies, GET Request to Retrieve All Movies
-app.get('/movies', (req, res)=>{
+// ROUTE HANDLER FUNCTIONS
+const getAllMovies = (req, res)=>{
     res.status(200).json({
         // json json formatting
         status: "success",
@@ -19,10 +18,8 @@ app.get('/movies', (req, res)=>{
             movies:movies
         }
     })
-})
-
-// GET Request to Retrieve a Single Movie by ID
-app.get('/movies/:id', (req, res) =>{
+}
+const getRouteId = (req, res) =>{
     // The id is extracted from the URL (req.params.id) and converted to a number using 
     const id = req.params.id * 1
     // FIND MOVIE BASED ON PRODUCT ID PARAMETER
@@ -43,10 +40,9 @@ app.get('/movies/:id', (req, res) =>{
             movie: movie
         }
     })
-})
-// POST - api/movies
-app.post('/movies', (req, res) => {
-     // extracting the id from the request using "params" then multiplying by 1 to make it a number
+}
+const createMovies = (req, res) => {
+    // extracting the id from the request using "params" then multiplying by 1 to make it a number
     const newID = movies[movies.length - 1].id + 1
     const newMovie = Object.assign({id: newID}, req.body)
     movies.push(newMovie)
@@ -58,9 +54,10 @@ app.post('/movies', (req, res) => {
             }
         })
     })
-})
-app.patch('/movies/:id', (req, res) => {
-     // extracting the id from the request using "params" then multiplying by 1 to make it a number
+}
+
+const updateMovie = (req, res) => {
+    // extracting the id from the request using "params" then multiplying by 1 to make it a number
     let id = req.params.id * 1
     let movieToUpdate  = movies.find(el => el.id === id)
     let index = movies.indexOf(movieToUpdate)
@@ -76,9 +73,9 @@ app.patch('/movies/:id', (req, res) => {
             }
         })
     })
-})
+}
 
-app.delete('/movies/:id', (req, res) =>{
+const deleteMovie = (req, res) =>{
     // extracting the id from the request using "params" then multiplying by 1 to make it a number
     const id = req.params.id * 1
     
@@ -106,7 +103,24 @@ app.delete('/movies/:id', (req, res) =>{
             }
         })
     })
-})
+}
+// api/movies
+/* app.get('/movies', getAllMovies)
+app.get('/movies/:id', getRouteId)
+app.post('/movies', createMovies)
+app.patch('/movies/:id', updateMovie)
+app.delete('/movies/:id', deleteMovie) */
+
+app.route('/movies')
+    .get(getAllMovies)
+    .post(createMovies)
+
+app.route('/movies:id')
+    .get(getAllMovies)
+    .patch(updateMovie)
+    .delete(deleteMovie)
+
+
 // Create Server
 const port = 3000
 app.listen(port, ()=>{
