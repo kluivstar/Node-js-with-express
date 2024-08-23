422a3e970589a9b2d89410006051956e7ee4e54e
Dev Denzel <100281304+devdenzel@users.noreply.github.com>
2024-08-23 12:08:09 +0100

    created a routes folder, placing route parameter functions and our endpoints within. which is then exported to the main App.js handling client's requests

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 // Setting Up Express and Reading the JSON File
 const express = require('express')
-const fs = require("fs")
 const morgan = require('morgan')
 const app = express()
-// Read file
-let movies = JSON.parse(fs.readFileSync("./data/movies.json"))
+
+const moviesRouter = require('./Routes/moviesRoutes')
+
 
 // Middleware that parse incoming JSON request
 const logger =  function(req, res, next) {
@@ -14,102 +14,7 @@ const logger =  function(req, res, next) {
 app.use(express.json())
 app.use(morgan('dev'))
 app.use(logger)
-// ROUTE HANDLER FUNCTIONS
-const getAllMovies = (req, res)=>{
-    res.status(200).json({
-        // json json formatting
-        status: "success",
-        requestAt: req.requestedAt,
-        data: {
-            movies:movies
-        }
-    })
-}
-const getRouteId = (req, res) =>{
-    // The id is extracted from the URL (req.params.id) and converted to a number using 
-    const id = req.params.id * 1
-    // FIND MOVIE BASED ON PRODUCT ID PARAMETER
-    let movie = movies.find(el => el.id === id)
 
-    // IF MOVIE IS NOT FOUND
-    if(!movie){
-        return res.status(404).json({
-            status: "fail",
-            data: {
-                message: `Movie with ${id} not found`
-            }
-        })
-    }
-    res.status(200).json({
-        status: "success",
-        data: {
-            movie: movie
-        }
-    })
-}
-const createMovies = (req, res) => {
-    // extracting the id from the request using "params" then multiplying by 1 to make it a number
-    const newID = movies[movies.length - 1].id + 1
-    const newMovie = Object.assign({id: newID}, req.body)
-    movies.push(newMovie)
-    fs.writeFile('./data/movies.json', JSON.stringify(movies), (err)=>{
-        res.status(201).json({
-            status: "success",
-            data: {
-                movies:newMovie
-            }
-        })
-    })
-}
-
-const updateMovie = (req, res) => {
-    // extracting the id from the request using "params" then multiplying by 1 to make it a number
-    let id = req.params.id * 1
-    let movieToUpdate  = movies.find(el => el.id === id)
-    let index = movies.indexOf(movieToUpdate)
-    Object.assign(movieToUpdate, req.body)
-
-    movies[index] = movieToUpdate
-    
-    fs.writeFile("./data/movies.json", JSON.stringify(movies), (err)=>{
-        res.status(200).json({
-            status: "success",
-            data: {
-                movie: movieToUpdate
-            }
-        })
-    })
-}
-
-const deleteMovie = (req, res) =>{
-    // extracting the id from the request using "params" then multiplying by 1 to make it a number
-    const id = req.params.id * 1
-    
-    // matching user route parameter id to id of movie array
-    const movieToDelete = movies.find(el=> el.id === id)
-    // If theres no movies to remove from the server
-    if(!movieToDelete){
-        return res.status(404).json({
-            status: "fail",
-            data: {
-                message: `Movie with ${id} not found`
-            }
-        })
-    }
-
-    const index = movies.indexOf(movieToDelete)
-    movies.splice(index, 1)
-
-    // when theres movies to remove from server
-    fs.writeFile('./data/movies.json', JSON.stringify(movies), (err) => {
-        res.send(204).json({
-            status: "success.",
-            data: {
-                movie: null
-            }
-        })
-    })
-}
 // api/movies
 /* app.get('/movies', getAllMovies)
 app.get('/movies/:id', getRouteId)
@@ -117,18 +22,8 @@ app.post('/movies', createMovies)
 app.patch('/movies/:id', updateMovie)
 app.delete('/movies/:id', deleteMovie) */
 
-const movieRouter = express.Router()
 
-movieRouter.route('/')
-    .get(getAllMovies)
-    .post(createMovies)
-
-movieRouter.route('/:id')
-    .get(getRouteId)
-    .patch(updateMovie)
-    .delete(deleteMovie)
-
-app.use('/movies', movieRouter)
+app.use('/movies', moviesRouter)
 // Create Server
 const port = 3000
 app.listen(port, ()=>{
