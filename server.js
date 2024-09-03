3f4b20a71448bbf6c3239f173c18ff519aaa10da
Dev Denzel <100281304+devdenzel@users.noreply.github.com>
2024-09-04 00:51:17 +0100

    createing schema and model

--- server.js
+++ server.js
@@ -9,11 +9,37 @@ const app = require('./app')
 // Create Server
 const port = process.env.PORT || 3000
 
+// connecting to remote DB
 mongoose.connect(process.env.CONN_STR, {
     useNewUrlParser: true
 }).then((conn) => {
     console.log('DB started something.')
+}).catch(err =>{
+    console.log('Some error has occured')
 })
+
+// creating a schema and model
+const movieSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Name is required'],
+        unique: true
+    },
+    description: String,
+    duration: {
+        type: Number,
+        required: [true, ['Duration is required']]
+    },
+    ratings: {
+        type: Number,
+        default: 1.0
+    }
+})
+
+// creating model with schema
+const movie = mongoose.model('Movie', movieSchema)
+
+// server
 app.listen(port, ()=>{
     console.log("Server wanna be startin something.")
 })
