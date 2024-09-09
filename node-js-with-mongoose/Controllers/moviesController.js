87def06ded8a6096ef1ad0986c2346c6cfd45800
Dev Denzel <100281304+devdenzel@users.noreply.github.com>
2024-09-09 02:25:07 +0100

    implementing aggregate pipelines match and group to get movies stats

--- node-js-with-mongoose/Controllers/moviesController.js
+++ node-js-with-mongoose/Controllers/moviesController.js
@@ -163,3 +163,35 @@ exports.deleteMovie = async (req, res) =>{
     }
 }
 
+exports.getMovieStats = async (req, res) => {
+    try{
+        const stats = await Movie.aggregate([
+            {$match: {ratings: {$gte: 8}}},
+            {$group: {
+                _id: '$releaseYear',
+                avgRating: { $avg: '$ratings'},
+                avgPrice: { $avg: '$price'},
+                minPrice: { $min: '$price'},
+                maxPrice: { $max: '$price'},
+                priceTotal: { $sum: '$price'},
+                movieCount: {$sum: 1}
+            }},
+            {$sort: {minPrice: 1}},
+            {$match: {maxPrice: {$gte: 2}}}
+        ])
+
+        res.status(200).json({
+            status: "success",
+            count: stats.length,
+            data: {
+                stats
+            }
+        })
+    }catch(err){
+        res.status(404).json({
+            status: "fail",
+            message: err.message
+        })
+    }
+}
+
