9896fc9d36e2c66f4155ce3b137fe9275cd3aacc
Dev Denzel <100281304+devdenzel@users.noreply.github.com>
2024-09-19 14:33:55 +0100

    created a User Schema and a User model

---
+++ 3-node-js-with-mongoose/Models/userModel.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const validator = require('validator')
+
+// user Schema
+const userSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: [true, "Please enter your name"]
+    },
+    email:{
+        type: String,
+        required: [true, 'Please enter your email'],
+        unique: true,
+        lowercase: true,
+        validate: [validator.isEmail, 'Please enter a valide email']
+    },
+    photo: String,
+    password: {
+        type: String,
+        required: [true, 'Please enter a password'],
+        minlength: 8
+    },
+    
+    confirmPassword: {
+        type: String,
+        required: [true, 'Please confirm your password']
+    }
+    
+})
+
+// user Model
+const User = mongoose.model('User', userSchema)
+
+module.exports = User;
