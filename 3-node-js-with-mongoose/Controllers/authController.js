336ad516a507277b77f73b2676de423fb49e769d
Dev Denzel <100281304+devdenzel@users.noreply.github.com>
2024-09-21 15:44:51 +0100

    signed up new user and handled server authentication using JWT

--- 3-node-js-with-mongoose/Controllers/authController.js
+++ 3-node-js-with-mongoose/Controllers/authController.js
@@ -1,13 +1,16 @@
 const User = require('./../Models/userModel')
 const asyncErrorHandler = require('./../Utils/asyncErrorHandler')
+const jwt = require('jsonwebtoken')
 
 // asyncErrorHandler catches error for exception i.e if a user is not created
 exports.signup = asyncErrorHandler(async (req, res, next) =>{
     const newUser = await User.create(req.body)
 
+    const token = jwt.sign({id: newUser._id}, process.env.SECRET_STR, {expiresIn: process.env.LOGIN_EXPIRES})
     // if a user is created
     res.status(201).json({
-        status: "success.",
+        status: "success",
+        token,
         data: {
             user: newUser
         }
